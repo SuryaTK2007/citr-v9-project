96bd14cb036b5c4adcbc878edfc3cb9ca2366b10
Surya <suryatk2007@gmail.com>
2026-06-11 18:09:30 +0530

    feat: update Pizza component to use default image and improve test cases

--- padre-ginos/src/__tests__/Pizza.spec.jsx
+++ padre-ginos/src/__tests__/Pizza.spec.jsx
@@ -1,14 +1,25 @@
-import { render } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 import Pizza from "../Pizza";
 import { expect, test } from "vitest"; 
 
 test("alt test renders on Pizza image", async () => {
     const name = "My Favourite Pizza";
-    const src = "https://picsum.photos/200/300";
+    const src = "https://picsum.photos/300/200";
     const screen = render(
         <Pizza name={name} description="super cool pizza" image={src} />
     );
     const img = screen.getByRole("img");
     expect(img.src).toBe(src);
     expect(img.alt).toBe(name);
+    cleanup();
 });
+test("to have default image if none is provided", async () => {
+    const name = "My Favourite Pizza";
+    const screen = render(
+        <Pizza name={name} description="super cool pizza" />
+    );
+    const img = screen.getByRole("img");
+    expect(img.src).toBe("https://picsum.photos/300/200");
+    expect(img.alt).toBe(name);
+    cleanup();
+}); 
